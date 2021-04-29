77999ba92f3ea64872a2ddd89a188fee404981b8
Aleksey <aleksey.amelchenko@gmail.com>
2021-04-29 12:32:37 +0300

    Initialize project using Create React App

---
+++ src/calculator/styled.js
@@ -0,0 +1,278 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  box-sizing: border-box;
+  flex-direction: column;
+  background-color: #2c3643;
+  box-shadow: 0px 2px 10px rgb(0 0 0 / 70%);
+`;
+export const Header = styled.div`
+  display: flex;
+  width: 100%;
+  height: 25px;
+  background-color: rgba(0, 0, 0, 0.2);
+`;
+export const Result = styled.div`
+  display: flex;
+  height: 130px;
+  background-color: #2c3643;
+  padding: 0px 20px;
+`;
+export const Current = styled.h1`
+  display: flex;
+  color: #fff;
+  font-size: 2.5em;
+  font-weight: 300;
+  margin-left: auto;
+`;
+export const Buttons = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  user-select: none;
+`;
+export const Clear = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 69px;
+  height: 69px;
+  margin-right: 2px;
+  background: #3a4654;
+  color: #727272;
+  font-size: 22px;
+  cursor: pointer;
+`;
+export const Back = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 69px;
+  height: 69px;
+  margin-right: 2px;
+  background: #3a4654;
+  color: #727272;
+  font-size: 22px;
+  cursor: pointer;
+`;
+export const Sqrt = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 69px;
+  height: 69px;
+  margin-right: 2px;
+  background: #3a4654;
+  color: #727272;
+  font-size: 22px;
+  cursor: pointer;
+`;
+export const Increase = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 69px;
+  height: 69px;
+  background: #3a4654;
+  color: #727272;
+  font-size: 22px;
+  cursor: pointer;
+`;
+export const Seven = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 69px;
+  height: 69px;
+  margin-right: 2px;
+  background: #3a4654;
+  color: #fff;
+  font-size: 22px;
+  cursor: pointer;
+`;
+export const Line = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 2px;
+  :last-child {
+    margin-bottom: 0;
+  }
+`;
+export const Eight = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 69px;
+  height: 69px;
+  margin-right: 2px;
+  background: #3a4654;
+  color: #fff;
+  font-size: 22px;
+  cursor: pointer;
+`;
+export const Nine = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 69px;
+  height: 69px;
+  margin-right: 2px;
+  background: #3a4654;
+  color: #fff;
+  font-size: 22px;
+  cursor: pointer;
+`;
+export const Split = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 69px;
+  height: 69px;
+  background: #3a4654;
+  color: #727272;
+  font-size: 22px;
+  cursor: pointer;
+`;
+export const Four = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 69px;
+  height: 69px;
+  margin-right: 2px;
+  background: #3a4654;
+  color: #fff;
+  font-size: 22px;
+  cursor: pointer;
+`;
+export const Five = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 69px;
+  height: 69px;
+  margin-right: 2px;
+  background: #3a4654;
+  color: #fff;
+  font-size: 22px;
+  cursor: pointer;
+`;
+export const Six = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 69px;
+  height: 69px;
+  margin-right: 2px;
+  background: #3a4654;
+  color: #fff;
+  font-size: 22px;
+  cursor: pointer;
+`;
+export const Minus = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 69px;
+  height: 69px;
+  background: #3a4654;
+  color: #727272;
+  font-size: 22px;
+  cursor: pointer;
+`;
+export const One = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 69px;
+  height: 69px;
+  margin-right: 2px;
+  background: #3a4654;
+  color: #fff;
+  font-size: 22px;
+  cursor: pointer;
+`;
+export const Two = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 69px;
+  height: 69px;
+  margin-right: 2px;
+  background: #3a4654;
+  color: #fff;
+  font-size: 22px;
+  cursor: pointer;
+`;
+export const Three = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 69px;
+  height: 69px;
+  margin-right: 2px;
+  background: #3a4654;
+  color: #fff;
+  font-size: 22px;
+  cursor: pointer;
+`;
+export const Plus = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 69px;
+  height: 69px;
+  background: #3a4654;
+  color: #727272;
+  font-size: 22px;
+  cursor: pointer;
+`;
+export const Zero = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 69px;
+  height: 69px;
+  margin-right: 2px;
+  background: #3a4654;
+  color: #fff;
+  font-size: 22px;
+  cursor: pointer;
+`;
+export const Comma = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 69px;
+  height: 69px;
+  margin-right: 2px;
+  background: #3a4654;
+  color: #fff;
+  font-size: 22px;
+  cursor: pointer;
+`;
+export const Percent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 69px;
+  height: 69px;
+  margin-right: 2px;
+  background: #3a4654;
+  color: #fff;
+  font-size: 22px;
+  cursor: pointer;
+`;
+export const Equals = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 69px;
+  height: 69px;
+  background: #3a4654;
+  color: #ff5252;
+  font-size: 22px;
+  cursor: pointer;
+`;
